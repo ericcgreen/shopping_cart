4be9c17c0639a80cd8a9b00c4308fc379023fba6
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-30 10:56:34 -0400

    testing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
 import App from './components/App';
+import Store from './Store'
 import './stylesheets/index.css';
 
+
+const StoreInstance = Store()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={StoreInstance}>
+    <App />,
+  </Provider>
   document.getElementById('root')
 );
